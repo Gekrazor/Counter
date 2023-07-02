c6e67aee6ee2eb2f89f33ebf3803beb5ec18da77
Leshchuk Dmitry <swtorgek@gmail.com>
2023-07-02 22:04:23 +0300

    Added count label, button to increase count label +1, code style fixes (standard version of task)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,13 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countButton: UIButton!
+    
+    
+    @IBAction func countLabelChange(_ sender: Any) {
+        if let text = countLabel.text {
+            let separetedText: Array = text.components(separatedBy: " ")
+            var valueToCount: Int = Int(separetedText.last!) ?? 0
+            valueToCount += 1
+            countLabel.text = "Значение счетчика: \(String(valueToCount))"
+            print(valueToCount)
+        } else {
+            print("Error! No Value")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Count to ten")
     }
-
-
 }
-
